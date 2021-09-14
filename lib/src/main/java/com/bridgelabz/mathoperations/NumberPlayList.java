ef156c8eab4db378a7fb88b7f0624e01fbd9e86d
srinadh1304 <srinadh1304@gmail.com>
2021-09-14 19:38:22 +0530

    MODIFIED: Finding minimum and maximum even number

--- lib/src/main/java/com/bridgelabz/mathoperations/NumberPlayList.java
+++ lib/src/main/java/com/bridgelabz/mathoperations/NumberPlayList.java
@@ -1,4 +1,5 @@
 package com.bridgelabz.mathoperations;
+
 import java.util.*;
 import java.util.function.Consumer;
 import java.util.function.Function;
@@ -11,76 +12,87 @@ public class NumberPlayList {
 	public static void main(String[] args) {
 		List<Integer> myNumberList = new ArrayList<Integer>();
 		for(int i=0;i<5;i++) myNumberList.add(i);
-		
+
 		Iterator<Integer> it = myNumberList.iterator();
 		while(it.hasNext()) {
 			Integer i =it.next();
 			System.out.println("Mth1: Iterator value : "+i);
 		}
-		
+
 		System.out.println();
 		class MyConsumer implements  Consumer<Integer>{
-				public void accept(Integer t) {
-					System.out.println("Mth2: consumer impl value : "+t);
-				}
+			public void accept(Integer t) {
+				System.out.println("Mth2: consumer impl value : "+t);
+			}
 		}
 		MyConsumer action = new MyConsumer();
 		myNumberList.forEach(action);
-		
+
 		System.out.println();
 		myNumberList.forEach(new Consumer<Integer>(){
 			public void accept(Integer t) {
 				System.out.println("Mth3: foreach anonymous class value : "+t);
 			}
 		});
-		
+
 		System.out.println();
 		Consumer<Integer> myListAction =  n -> {
 			System.out.println("Mth4: foreach lambda impl value : "+ n);
 		};
 		myNumberList.forEach(myListAction);
-		
-		
+
+
 		System.out.println();
 		myNumberList.forEach(n->{
 			System.out.println("Mth5: foreach lambda impl value : "+ n);
 		});
-		
+
 		System.out.println();
 		Function<Integer, Double> toDoubleFunction = Integer::doubleValue;
 		myNumberList.forEach(n->{
 			System.out.println("Mth5: foreach lambda double value : "+ toDoubleFunction.apply(n));
 		});
-		
+
 		System.out.println();
 		Predicate<Integer> isEvenFunction = n ->  n%2==0;
 		myNumberList.forEach(n->{
 			System.out.println("Mth5: foreach value of : "+n+" check for even : " +isEvenFunction.test(n));
 		});
-		
-		
+
+
 		System.out.println();
 		myNumberList.stream().forEach(n ->{
 			System.out.println("Mth8: stream forEach value : "+n);
 		});
-		
+
 		myNumberList.stream().map(toDoubleFunction).forEach(System.out::println);
 		List<Double> doubleList = myNumberList.stream()
-								  .map(toDoubleFunction)
-								  .collect(Collectors.toList());
+				.map(toDoubleFunction)
+				.collect(Collectors.toList());
 		System.out.println("Printing Double list: "+doubleList);
-		
-		
+
+
 		List<Integer> evenList = myNumberList.stream()
-								 .filter(isEvenFunction)
-								 .collect(Collectors.toList());
+				.filter(isEvenFunction)
+				.collect(Collectors.toList());
 		System.out.println("Printing Even List : "+evenList);	
-		
+
 		Integer firstEven = myNumberList.stream()
-							.filter(isEvenFunction)
-							.peek(n-> System.out.println("Peek even number "+ n))
-							.findFirst()
-							.orElse(null);
+				.filter(isEvenFunction)
+				.peek(n-> System.out.println("Peek even number "+ n))
+				.findFirst()
+				.orElse(null);
 		System.out.println("the first even is : "+ firstEven);
- 	}
+
+		Integer min = myNumberList.stream()
+				.filter(isEvenFunction)
+				.min((x,y) -> x-y).orElse(null);
+		System.out.println("the min value is : "+min);
+
+		Integer max = myNumberList.stream()
+				.filter(isEvenFunction)
+				.max((x,y) -> x-y).orElse(null);
+		System.out.println("the min value is : "+max);
+
+	}
 }
